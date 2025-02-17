783c8d780ab85b53ad66cf5d54cfa3355d168724
voidcht <chathurangilansakara@gmail.com>
2025-02-18 02:19:02 +0530

    Tutorial

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+
+  firstName : string = "Angular tutorial";
+  angularVersion = "Version 18";
+
+  version : number = 18;
+
+  isActive : boolean = true;
+
+  currentDate : Date = new Date();
+
+  inputType : string = "checkbox";
+
+  selectedCity : string = '';
+
+  showWelcomeAlert(){
+    alert("Welcome to Angular 18!");
+  }
+
+  showMessage(message : string){
+    alert(message);
+  }
+
 
 }
