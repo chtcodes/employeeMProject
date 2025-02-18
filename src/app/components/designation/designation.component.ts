28faa9c9ebd4a5c085e3441aa35fd838b94a7a57
voidcht <chathurangilansakara@gmail.com>
2025-02-19 00:21:48 +0530

    Navigation bar added

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -12,12 +12,15 @@ import { IAPIResponseModel, IDesignation } from '../../model/interface/role';
 export class DesignationComponent implements OnInit{
   designationList : IDesignation[] = [];
   masterService = inject(MasterService);
+  isLoader : boolean = true;
 
   ngOnInit(): void {
     this.masterService.getDesignations().subscribe((result : IAPIResponseModel)=>{
       this.designationList= result.data;
+      this.isLoader = false;
     },error=>{
-      alert("API error! The network may be down!")
+      alert("API error! The network may be down!");
+      this.isLoader = false;
     });
   }
 
