7d17b4ed0065fa09ed0c7bbf361c362aa088f2e8
voidcht <chathurangilansakara@gmail.com>
2025-02-26 19:44:49 +0530

    Route guard

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const localUser = localStorage.getItem('empERPUser');
+  const router = inject(Router);
+  
+  if(localUser!= null){
+    return true;
+  }else{
+    router.navigateByUrl('/login');
+    return false; 
+  }
+
+  
 };
